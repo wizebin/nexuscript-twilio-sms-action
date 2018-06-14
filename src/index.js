16c2a1e4bd41ef3930779579fd0b11cbd89a5eae
Stephen <wizebin@gmail.com>
2018-06-14 13:04:26 -0400

    Added first iteration of accounts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { wrapAction } from 'nexusdk';
 import twilio from 'twilio';
 
-wrapAction(async (properties, sendMessage) => {
-  const { account_sid, auth_token, from, to, body } = properties;
+wrapAction(async (properties) => {
+  const { twilio_account, account_sid, body } = properties;
+  let { auth_token, from, to } = (twilio_account || {});
+  if (!auth_token) ({ auth_token, from, to } = properties);
 
   const client = new twilio(account_sid, auth_token);
 
