5fafae3afbe99640634098dcbda9e7fc807dab2d
Stephen <wizebin@gmail.com>
2018-06-16 03:05:53 -0400

    Cleanup, working account version

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import { get, getTypeString } from 'objer';
 function formatNumber(number) {
   const typeString = getTypeString(number);
   if (typeString === 'string') {
-    const justNumbers = typeString.replace(/\D/g, '');
+    const justNumbers = number.replace(/\D/g, '');
     if (justNumbers.length === '12223334444'.length) {
       return `+${justNumbers}`;
     } else if (justNumbers.length === '2223334444'.length) {
       return `+1${justNumbers}`;
     }
-    throw new Error(`Malformed phone number ${number}`);
+    throw new Error(`Malformed phone number ${number} ${justNumbers}`);
   }
   throw new Error(`Somehow passed phone number is not a string but is instead ${typeString}`);
 }
 
 wrapAction(async (properties) => {
-  const { account_sid, auth_token, from } = get(properties, ['accounts', 'twillio_account', 'auth']) || {};
+  const { account_sid, auth_token, from } = get(properties, ['accounts', 'twilio_account', 'auth']) || {};
   const { to, body } = get(properties, ['data']) || {};
 
   const client = new twilio(account_sid, auth_token);
